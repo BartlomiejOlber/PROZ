ec80ec4b571e89548dd8c3f8346a96cd61a62e80
Adrian Prorok <adrian.prorok99@gmail.com>
2020-05-24 22:41:13 +0200

    Little changes in MyMenuBar and documentation.

--- src/main/java/edu/proz/checkers/client/view/MyMenuBar.java
+++ src/main/java/edu/proz/checkers/client/view/MyMenuBar.java
@@ -3,6 +3,9 @@ package edu.proz.checkers.client.view;
 import javax.swing.*;
 import java.awt.event.ActionListener;
 
+/**
+ * Class that makes it possible to add menu bar in our gui frame
+ */
 public class MyMenuBar extends JMenuBar {
 
     private JMenu gameOptions;
@@ -12,38 +15,65 @@ public class MyMenuBar extends JMenuBar {
     private JMenuItem keyboardShortcuts;
     private JMenuItem aboutTheGame;
 
+    private final String gameOptionsText = "Game options";
+    private final String exitText = "Exit";
+    private final String helpText = "Help";
+    private final String keyboardShortcutsText = "Keyboard shortcuts";
+    private final String aboutTheGameText = "About the game";
+
+    private final String exitKeyStroke = "alt F4";
+    private final String keyboardShortcutsKeyStroke = "ctrl K";
+    private final String aboutTheGameKeyStroke = "ctrl A";
+
+    /**
+     * Constructor that creates menu items and adds them to the bar.
+     */
     public MyMenuBar() {
-        gameOptions = new JMenu("Game options");
+        gameOptions = new JMenu(gameOptionsText);
         this.add(gameOptions);
-        exit = new JMenuItem("Exit");
+        exit = new JMenuItem(exitText);
         gameOptions.add(exit);
 
-        help = new JMenu("Help");
+        help = new JMenu(helpText);
         this.add(help);
-        keyboardShortcuts = new JMenuItem("Keyboard shortcuts");
-        aboutTheGame = new JMenuItem("About the game");
+        keyboardShortcuts = new JMenuItem(keyboardShortcutsText);
+        aboutTheGame = new JMenuItem(aboutTheGameText);
         help.add(keyboardShortcuts);
         help.addSeparator();
         help.add(aboutTheGame);
     }
 
+    /**
+     * Method that assigns the action listener to all the options in menu bar.
+     * @param object Object that will be added as action listener.
+     */
     public void setActionListener(ActionListener object) {
         this.exit.addActionListener(object);
         this.keyboardShortcuts.addActionListener(object);
         this.aboutTheGame.addActionListener(object);
     }
 
+    /**
+     * Method that assigns keyboard shortcuts to all the options in menu bar.
+     */
     public void setKeyboardShortcuts() {
-        this.exit.setAccelerator(KeyStroke.getKeyStroke("alt F4"));
-        this.keyboardShortcuts.setAccelerator(KeyStroke.getKeyStroke("ctrl K"));
-        this.aboutTheGame.setAccelerator(KeyStroke.getKeyStroke("ctrl A"));
+        this.exit.setAccelerator(KeyStroke.getKeyStroke(exitKeyStroke));
+        this.keyboardShortcuts.setAccelerator(KeyStroke.getKeyStroke(keyboardShortcutsKeyStroke));
+        this.aboutTheGame.setAccelerator(KeyStroke.getKeyStroke(aboutTheGameKeyStroke));
     }
 
-
+    /**
+     * Method that returns menu item that represents exit option.
+     * @return Menu item that represents exit option.
+     */
     public JMenuItem getExit() {
         return this.exit;
     }
 
+    /**
+     * Method that returns menu item that represents get keyboard shortcuts option.
+     * @return Menu item that represents get keyboard shortcuts option.
+     */
     public JMenuItem getKeyboardShortcuts() {
         return this.keyboardShortcuts;
     }
